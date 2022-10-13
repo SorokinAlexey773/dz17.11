d5ba86b62b96d56a93029334884100cec52ab0b4
alexey <sorokinnn73@mail.ru>
2022-10-14 00:46:43 +0400

    сделали 4 задачу

---
+++ 13.10/задача4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+else 
+{
+    max = max;
+}
+if (max < c)
+{
+    max = c;
+}
+else 
+{
+    max = max;
+}
+Console.Write($"Макс. число {max}");
