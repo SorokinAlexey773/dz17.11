cce443eb946bd1ce7bde4cfe6875a5b166cc2366
alexey <sorokinnn73@mail.ru>
2022-10-14 22:59:03 +0400

    сделал 8 задачу

---
+++ 13.10/задача8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= N)
+{
+    if (a % 2 == 0)
+    {
+       Console.Write($"{a} , ");
+      a = a + 1; 
+    }
+    else 
+    {
+        a = a + 1;
+    }
+
+}
+
+
+
+
+    
+
