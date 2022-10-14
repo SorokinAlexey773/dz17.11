7bac56eaad2d663e27176fb85dfb03e434a39474
alexey <sorokinnn73@mail.ru>
2022-10-14 22:16:11 +0400

    сделал 6 задачу

--- задача6/Program.cs
+++ задача6/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2)
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
 {
  Console.Write("да");
 }
