2baf36faf8404f269cf4c72da82a740efefd107f
alexey <sorokinnn73@mail.ru>
2022-10-22 00:23:24 +0400

    end

---
+++ Новая папка/task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// // 918 -> 1 
+int SecondDigit (int number)
+{
+number = number / 10;
+ return number = number % 10;
+}
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = num * - 1;
+}
+int secondDigit = SecondDigit(num);
+Console.WriteLine(num > 1000 ? "Некорректное число" : $"Вторая цифра числа {num} = {secondDigit} ");
---
+++ Новая папка/task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+bool Week(int number)
+{
+  return  number == 6 || number == 7;
+}
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 7)
+{
+    Console.WriteLine("Некорректное число");
+}
+bool week = Week(num);
+Console.WriteLine(week ? $"{num} -> да" : $"{num} -> нет" );
